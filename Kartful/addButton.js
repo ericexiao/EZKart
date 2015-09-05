d202837290cc68b4015c9b4e9c28ad399f7aa786
kyq3ar <kyq3ar@virginia.edu>
2015-09-05 15:45:19 -0400

    Ajax for PHP

--- Kartful/addButton.js
+++ Kartful/addButton.js
@@ -3,7 +3,20 @@ $(document).ready(function() {
 	if (url.indexOf("amazon") >= 0)
 	{
 		//var button = "<button id=\"addtoCart\" type = \"submit\" class =\"btn btn-default\"><img src=\"icon.png\"></button>";
-		var button = "<a href=\"#addtoCart\" class=\"btn btn-info\"><span class=\"glyphicon glyphicon-shopping-cart\">Add</span></a>";
+		var button = "<button id=\"#addtoCart\" type=\"button\" class=\"btn btn-info\"><span class=\"glyphicon glyphicon-shopping-cart\">Add</span></button>";
 		$(button).insertAfter("#title_feature_div");
 	}
+	$('body').on('click', 'button.btn', function(){
+  		$.ajax({
+    		type: "POST",
+  			url: "awsScript.php",
+  			data: {
+  				url: url;
+  			},
+  			contentType: "application/json; charset=utf-8",
+  			success: function(r) {
+  				alert("YAY");
+  	  }
+		});
+	});
 });
