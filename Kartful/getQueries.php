81c6f34f2d532d5c27781e15cf57d613995ed643
kyq3ar <kyq3ar@virginia.edu>
2015-09-06 00:23:10 -0400

    GetQueries included.

--- Kartful/getQueries.php
+++ Kartful/getQueries.php
@@ -1,2 +1,26 @@
 <?php
+	$db = new mysqli('localhost', "ericineo_kartful", "Penn123!", 'ericineo_kartful');
+	if (mysqli_connect_errno())
+	{
+		echo "Connection Error!";
+		return;
+	}
+	
+	$stmt = $db->stmt_init();
+    if ($stmt->prepare("SELECT productURL, productName, price, imageURL FROM userProducts")){
+   		$stmt->execute();
+   		$stmt->bind_result($productURL, $productName, $price, $imageURL);
+
+   		while($stmt->fetch()) {
+   			echo ("<a style=\"display:block\" href=\" . $productURL . \">\n");
+   			echo "<div class = \"square\">\n";
+   			echo ("<h1>" . $productName . "</h1>\n");
+   			echo ("<h2>" . $price . "</h2>\n");
+   			echo ("<hr></hr>\n");
+   			echo ("img class = \"square\" style = \"height:80%; width:80%\" src = \" . $imageURL . \"\n");
+   			echo ("</div>\n");
+   			echo ("</a\n");
+   		}
+    }
+    mysqli_close($db);
 ?>
