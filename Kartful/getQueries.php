c0906173743931bf2101f1d0226cbd1cc7eaf4a2
ericexiao <es2py@virginia.edu>
2015-09-06 02:47:22 -0400

    debugged the web interface of the extension

--- Kartful/getQueries.php
+++ Kartful/getQueries.php
@@ -10,16 +10,19 @@
     if ($stmt->prepare("SELECT productURL, productName, price, imageURL FROM userProducts")){
    		$stmt->execute();
    		$stmt->bind_result($productURL, $productName, $price, $imageURL);
+   		while( $stmt->fetch()) {
+   			if (trim($productName) != "") {
+	   			echo ("<a style=\"display:block\" href=\"" . $productURL . "\">\n");
+	   			echo "<div class = \"square\">\n";
+	   			echo ("<h1>" . $productName . "</h1>\n");
+	   			echo ("<h2>" . $price . "</h2>\n");
+	   			echo ("<hr></hr>\n");
+	   			$imageURL = "http://" . $imageURL;
+	   			echo ("<img class = \"square\" style = \"height:80%; width:80%\" src = \"" . $imageURL . "\">\n");
+	   			echo ("</div>\n");
+	   			echo ("</a>\n");
 
-   		while($stmt->fetch()) {
-   			echo ("<a style=\"display:block\" href=\"" . $productURL . "\">\n");
-   			echo "<div class = \"square\">\n";
-   			echo ("<h1>" . $productName . "</h1>\n");
-   			echo ("<h2>" . $price . "</h2>\n");
-   			echo ("<hr></hr>\n");
-   			echo ("<img class = \"square\" style = \"height:80%; width:80%\" src = \"" . $imageURL . "\">\n");
-   			echo ("</div>\n");
-   			echo ("</a>\n");
+	   		}
    		}
     }
     mysqli_close($db);
