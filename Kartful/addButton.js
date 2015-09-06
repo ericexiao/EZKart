e0402e9a8e970d2fea6ee8c2918568b78175ac69
kyq3ar <kyq3ar@virginia.edu>
2015-09-06 01:56:24 -0400

    Fixed button on amazon.com.

--- Kartful/addButton.js
+++ Kartful/addButton.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
 	if (productURL.indexOf("amazon") >= 0)
 	{
 		//var button = "<button id=\"addtoCart\" type = \"submit\" class =\"btn btn-default\"><img src=\"icon.png\"></button>";
-		var button = "<button id=\"#addtoCart\" type=\"button\" class=\"btn btn-info\"><span class=\"glyphicon glyphicon-shopping-cart\">Add</span></button>";
+		var button = "<button id=\"addtoCart\" type=\"button\" class=\"btn btn-info\"><span class=\"glyphicon glyphicon-shopping-cart\"></span></button>";
 		$(button).insertAfter("#title_feature_div");
+		$('head').append('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">');
+		$('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>');
+		$('head').append('<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>');
+		$('#addtoCart').css('font-family', 'Varela Round');
 	}
 	$('body').on('click', 'button.btn', function(){
   		$.ajax({
